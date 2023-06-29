037f3992435693dc1c52c4a4e4f502491d566740
Siyung <siyungcho@gmail.com>
2023-06-29 16:58:56 -0400

    changed layout and files

--- script.js
+++ script.js
@@ -3,3 +3,38 @@ document.addEventListener("mousemove", function(event) {
     glow.style.left = event.clientX + "px";
     glow.style.top = event.clientY + "px";
 });
+
+$(document).ready(function(){
+    $("#Main-Section-About").show()
+    $("#Main-Section-Projects").hide()
+    $("#Main-Section-Experience").hide()
+    $("#Main-Section-Education").hide()
+});
+
+$('#About-Button').click(function(){
+    $("#Main-Section-About").show()
+    $("#Main-Section-Projects").hide()
+    $("#Main-Section-Experience").hide()
+    $("#Main-Section-Education").hide()
+});
+
+$('#Projects-Button').click(function(){
+    $("#Main-Section-About").hide()
+    $("#Main-Section-Projects").show()
+    $("#Main-Section-Experience").hide()
+    $("#Main-Section-Education").hide()
+});
+
+$('#Experience-Button').click(function(){
+    $("#Main-Section-About").hide()
+    $("#Main-Section-Projects").hide()
+    $("#Main-Section-Experience").show()
+    $("#Main-Section-Education").hide()
+});
+
+$('#Education-Button').click(function(){
+    $("#Main-Section-About").hide()
+    $("#Main-Section-Projects").hide()
+    $("#Main-Section-Experience").hide()
+    $("#Main-Section-Education").show()
+});
